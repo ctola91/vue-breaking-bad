94e51ec000649db09961ce905dd837f18606d3ce
ctola91 <ctolapacheco@gmail.com>
2023-02-26 23:01:40 -0400

    remove break images

--- src/characters/components/CharacterCard.vue
+++ src/characters/components/CharacterCard.vue
@@ -10,10 +10,19 @@ const props = defineProps<Props>();
 const character = toRef(props, "character");
 </script>
 <template>
-  <div class="character-card">
+  <div class="character-card" v-if="[14, 17, 39].includes(character.char_id)">
     <img :src="character.img" :alt="character.name" />
     <h3>{{ character.name }}</h3>
   </div>
+  <!-- <div class="character-card">
+    <img
+      v-if="[14, 17, 39].includes(character.char_id)"
+      src="https://crescenttide.com/wp-content/uploads/2019/07/no-avatar-300x300.png"
+      alt="No Name"
+    />
+    <img v-else :src="character.img" :alt="character.name" />
+    <h3>{{ character.name }}</h3>
+  </div> -->
 </template>
 
 <style scoped>
