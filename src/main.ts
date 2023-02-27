1ed0c07d9fc460fd406c331e567e1cf56ea5e045
ctola91 <ctolapacheco@gmail.com>
2023-02-26 21:17:37 -0400

    Add vue-query

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
-import router from './router';
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import router from "./router";
 import App from "./App.vue";
 
 import "./assets/main.css";
 
 const app = createApp(App);
+
+app.use(VueQueryPlugin);
 
 app.use(router);
 app.mount("#app");
