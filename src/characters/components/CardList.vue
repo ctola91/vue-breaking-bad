949fa63f5f591a3c3b6d00417f41ca26330175a1
ctola91 <ctolapacheco@gmail.com>
2023-02-26 21:41:18 -0400

    initialize endpoint using vue-query

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -1,16 +1,34 @@
 <script setup lang="ts">
+import { useQuery } from "@tanstack/vue-query";
 import { useCharacters } from "@/characters/composables/useCharacters";
+import breakingBadApi from "@/api/breakingBadApi";
+import type { Character } from "../interfaces/character";
 
+//! 1- Normal Suspense
 // Con Suspense
 // const { data } = await breakingBadApi.get<Character[]>("/characters");
 // const characters = ref<Character[]>(data);
-// Sin Suspense
 
-const { isLoading, characters, hasError, errorMessage } = useCharacters();
+//! 2- Composable functions
+// Sin Suspense
+// const { isLoading, characters, hasError, errorMessage } = useCharacters();
+
+const getCharactersSlow = async (): Promise<Character[]> => {
+  const { data } = await breakingBadApi.get<Character[]>("/characters");
+  return data;
+};
+
+//! 3- TanStack Query
+const {
+  isLoading,
+  isError,
+  data: characters,
+  error,
+} = useQuery(["characters"], getCharactersSlow, { cacheTime: 1000 * 60 });
 </script>
 <template>
   <h1 v-if="isLoading">Loading</h1>
-  <h1 v-if="hasError || errorMessage">ERROR!!!</h1>
+  <h1 v-if="isError">{{ error }}</h1>
   <ul>
     <li v-for="{ char_id, name } of characters" :key="char_id">
       {{ name }}
