76aeaca06d6d887e3739ce8c1479b33703cb2f1d
ctola91 <ctolapacheco@gmail.com>
2023-03-02 21:53:20 -0400

    add ajax call and cache implementation with useQuery

--- src/characters/pages/CharacterId.vue
+++ src/characters/pages/CharacterId.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
+import type { Character } from "@/characters/interfaces/character";
+import characterStore from "../../store/characters.store";
+import breakingBadApi from "@/api/breakingBadApi";
+import { useQuery } from "@tanstack/vue-query";
 
 const route = useRoute();
 const { id } = route.params as { id: string };
+
+const getCharacterCacheFirst = async (
+  characterId: string
+): Promise<Character> => {
+  if (characterStore.checkIdInStore(characterId)) {
+    return characterStore.ids.list[characterId];
+  }
+  const { data } = await breakingBadApi.get<Character[]>(
+    `/characters/${characterId}`
+  );
+  return data[0];
+};
+
+const { data } = useQuery(
+  ["characters", id],
+  () => getCharacterCacheFirst(id),
+  {
+    onSuccess(character) {
+      characterStore.loadedCharacter(character);
+    },
+  }
+);
 </script>
 <template>
   <div>
@@ -10,6 +36,7 @@ const { id } = route.params as { id: string };
     <!-- <h1>Character {{ route.params.id }}</h1> -->
     <!-- Se pierde la reactividad, usar para componentes stateless generalmente -->
     <h1>Character {{ id }}</h1>
+    <code>{{ data }}</code>
   </div>
 </template>
 <style scoped></style>
